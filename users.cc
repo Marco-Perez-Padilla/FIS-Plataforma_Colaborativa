2ab87dae1336596cf69c0ffccde0375968203e30
usuario <alu0101469348@ull.edu.es>
2025-04-23 12:54:03 +0000

    Add basics of class User

---
+++ users.cc
@@ -0,0 +1,19 @@
+/**
+** Universidad de La Laguna
+** Escuela Superior de Ingenieria y Tecnologia
+** Grado en Ingenieria Informatica
+** Asignatura: Fundamentos de la Ingeniería del Software
+** Curso: 2º
+** Practica 6: Entrega de desarrollo ágil
+** Autores: Marco Pérez Padilla, /////
+** Correo: alu0101469348@ull.edu.es
+** Fecha: 23/04/2025
+
+** Archivo users.cc: Implementacion de la clase User
+**
+** Referencias:
+**      Enlaces de interes
+
+** Historial de revisiones:
+**      23/04/2025 - Creacion (primera version) del codigo
+**/
